d1c8c3d645842e31508ed5abd44db464c9990771
ZurcAR <artesafo@gmail.com>
2023-11-07 16:49:27 -0300

    Update Teste.cs

--- Assets/Teste.cs
+++ Assets/Teste.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Teste : MonoBehaviour
 {
     [SerializeField] string _nome;
+    [SerializeField] int _number;
+    [SerializeField] int _number2;
+
     // Start is called before the first frame update
     void Start()
     {
