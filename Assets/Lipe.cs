478000d15072e9a6568eedb994dfdb690c141f77
Luis Rodrigues <luis52016636@edu.pa.senac.br>
2023-11-07 17:14:15 -0300

    lipe

--- Assets/Lipe.cs
+++ Assets/Lipe.cs
@@ -2,13 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Teste : MonoBehaviour
+public class Lipe : MonoBehaviour
 {
-    [SerializeField] string _nome;
-    [SerializeField] int _number;
-    [SerializeField] float _dec;
-    [SerializeField] bool _check;
-
     // Start is called before the first frame update
     void Start()
     {
